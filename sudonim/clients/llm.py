17dc217f77ace8a986ed2fdefad4124a058e11f2
Dustin Franklin <dustinf@nvidia.com>
2025-05-07 05:37:00 -0400

    added <thinking> tokens in stream output

--- sudonim/clients/llm.py
+++ sudonim/clients/llm.py
@@ -13,7 +13,7 @@ models = [x.id for x in client.models.list()]
 config = {
   'model': os.environ.get('MODEL', models[0] if models else 'default'),
   'stream': True,
-  'stream_options': {'include_usage': True}
+  'stream_options': {'include_usage': True},
 }
 
 if 'MAX_TOKENS' in os.environ:
@@ -23,8 +23,7 @@ print(f"Connected to server: {client.base_url}")
 print(f"Found served models: {', '.join(models)}")
 print(f"Generation config:   {' '.join([str(k)+'='+str(v) for k,v in config.items()])}")
 
-# generate responses to some default prompts
-prompts = [
+prompts = [  # generate responses to some default prompts
   "Why did the LLM cross the road?",
   "If a train travels 120 miles in 2 hours, what is its average speed?",
   "Alice’s brother was half her age when she was 6. How old is her brother when she’s 42?",
@@ -33,40 +32,46 @@ prompts = [
 ]
 
 for prompt in prompts:
-  time_new = time.perf_counter()
-  messages = [{
-    'role': 'user',
-    'content': prompt
-  }]
+  time_beg = time.perf_counter()
+  time_tft = None  # Time To First Token latency (TTFT)
 
-  text_reply = usage = ''
-  completion = client.chat.completions.create(messages=messages, **config)
+  completion = client.chat.completions.create(  # send generation request
+    messages=[{
+      'role': 'user',
+      'content': prompt
+    }], **config
+  )
 
   print(f"\n\033[94m{prompt}\033[00m\n")
   
   for chunk in completion:
-    if not chunk.choices: # the last chunk has usage, not reply choices
+    if not chunk.choices: # the last chunk has usage stats (not replies)
       usage = chunk.usage
       continue
 
-    delta = chunk.choices[0].delta.content
+    delta = chunk.choices[0].delta 
+    content = delta.content
 
-    if not delta:
-      continue
+    if not content: # check for <thinking> tokens
+      reasoning = getattr(delta, 'reasoning_content', None)
 
-    if not text_reply: # record the Time to First Token latency
-      time_first = time.perf_counter()
+      if not reasoning:
+        continue
 
-    text_reply += delta
-    print(delta, end='')
+      content = f'\033[2m{reasoning}\033[22m'  # dim CoT
 
-  time_end = time.perf_counter()
-  time_ftl = time_first-time_new
-  time_gen = time_end-time_first
+    if time_tft is None: # this was the first output token
+      time_tft = time.perf_counter()
+
+    print(content, end='', flush=True)
+
+  # print performance metrics
+  time_gen = time.perf_counter() - time_tft
+  time_tft = time_tft - time_beg
 
   print(f"\n\n\033[32m> {config['model']} | " + " | ".join([
-    f"Prefill: {usage.prompt_tokens} tokens @ {usage.prompt_tokens/time_ftl:.2f} t/s" if usage else '',
-    f"Time to First Token: {time_ftl:.2f}s",
+    f"Prefill: {usage.prompt_tokens} tokens @ {usage.prompt_tokens/time_tft:.2f} t/s" if usage else '',
+    f"Time to First Token: {time_tft:.2f}s",
     f"Generation: {usage.completion_tokens} tokens @ {usage.completion_tokens/time_gen:.2f} t/s" if usage else '',
-    f"Total: {time_end-time_new:.2f}s",
+    f"Total: {time_tft + time_gen:.2f}s",
   ]) + " \033[00m")
